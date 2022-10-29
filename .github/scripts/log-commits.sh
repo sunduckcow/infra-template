26e26abc2b82add33b7ab88f37588658a7234946
Nikita Sandakov <sunduckcow@gmail.com>
2022-10-29 03:53:53 +0300

    finaly

--- .github/scripts/log-commits.sh
+++ .github/scripts/log-commits.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 'TEST!!'
-#echo "Ответственный за релиз $(git log --pretty=format:'%an'  @^..@)"
-#echo "коммиты, попавшие в релиз:"
-#git log --pretty=format:'%h %an %s' $(git describe --tags @^ --abbrev=0)..
+
+echo "Ответственный за релиз $(git log --pretty=format:'%an'  @^..@)"
+echo "коммиты, попавшие в релиз:"
+git log --pretty=format:'%h %an %s' $(git describe --tags @^ --abbrev=0)..
