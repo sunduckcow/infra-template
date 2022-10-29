e1fdef350b984b5c07ac9b014181736ffe1f11d9
Nikita Sandakov <sunduckcow@gmail.com>
2022-10-29 03:51:31 +0300

    added script file

---
+++ .github/scripts/log-commits.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Ответственный за релиз $(git log --pretty=format:'%an'  @^..@)"
+echo "коммиты, попавшие в релиз:"
+git log --pretty=format:'%h %an %s' $(git describe --tags @^ --abbrev=0)..
