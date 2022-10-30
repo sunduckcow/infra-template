7957ab48dee9b872f0482aefd05c90950a2f8253
Nikita Sandakov <sunduckcow@gmail.com>
2022-10-30 19:59:27 +0300

    trying to send comments

--- .github/scripts/send-message.sh
+++ .github/scripts/send-message.sh
@@ -2,10 +2,13 @@
 
 body=$1
 
+ticket="HOMEWORKSHRI-173"
+link="https://api.tracker.yandex.net/v2/issues/$ticket/$2"
+
 curl \
 -w "%{http_code}" \
 --data-raw "$body" \
 -H "Content-Type: application/json"  \
 -H "Authorization: OAuth ${OAuth}" \
 -H "X-Org-ID: ${orgId}" \
--X PATCH 'https://api.tracker.yandex.net/v2/issues/HOMEWORKSHRI-173'
+-X PATCH $link
