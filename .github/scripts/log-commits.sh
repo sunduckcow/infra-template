c0772e62bef94e7f093e6e71ac580f0f288e67ed
Nikita Sandakov <sunduckcow@gmail.com>
2022-10-30 16:10:29 +0300

    trying tickets creation

--- .github/scripts/log-commits.sh
+++ .github/scripts/log-commits.sh
@@ -3,15 +3,15 @@
 #logs all commits since last tag or from the begining
 
 author=`git log --pretty=format:'%an'  @^..@`
-echo "Ответственный за релиз $author"
-echo "коммиты, попавшие в релиз:"
 
 nTags=`git tag | wc -w | xargs`
 
 if [ $nTags -gt "1" ] # nTags > 1
 then
 last_tag=`git describe --tags @^ --abbrev=0` #get last tag name before current
-git log --pretty=format:'%h %an %s\n' $last_tag.. | tr -s '\n' ' ' #log all commits after prew tag
+logs=$(git log --pretty=format:'\n%h %an %s' $last_tag.. | tr -d '\n') #log all commits after prew tag
 else
-git log --pretty=format:'%h %an %s' #just log all commits
+logs=$(git log --pretty=format:'\n%h %an %s' | tr -d '\n') #just log all commits
 fi
+
+echo "Ответственный за релиз $author\nкоммиты, попавшие в релиз:$logs"
--- .github/scripts/send-message.sh
+++ .github/scripts/send-message.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-body='{"description": "'"$1"'"}'
+message=$1
 
-Auth="y0_AgAAAAA_tfeeAAiJqgAAAADSlurD7DXQzzAeS_KNK7VMEHVaL7u2dx4"
-orgId="7526988"
+body='{"description": "'"$message"'"}'
 
 curl \
 -w "%{http_code}" \
 --data-raw "$body" \
 -H "Content-Type: application/json"  \
--H "Authorization: OAuth ${Auth}" \
+-H "Authorization: OAuth ${OAuth}" \
 -H "X-Org-ID: ${orgId}" \
 -X PATCH 'https://api.tracker.yandex.net/v2/issues/HOMEWORKSHRI-173'
