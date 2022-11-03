eb38e1fad62252d8766ce558503048cb65e2a3f4
Якушевский Артём <artem19o@yandex.ru>
2022-11-03 18:32:03 +0300

    broke linter & types

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var a: number = 2;
+  a = 'asg';
+
   return (
     <div className="App">
       <header className="App-header">
